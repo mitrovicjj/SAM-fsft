2850f1a4f80bf721e8d46843258efd75d019cb45
mitrovicjj <mitrovicj1012@gmail.com>
2025-07-31 16:36:05 +0200

    Added utils back

--- data/utils.py
+++ data/utils.py
@@ -1,25 +1,33 @@
 import os
 from torch.utils.data import DataLoader
-from datasets import RetinaDataset, get_transforms
+from data.dataset import RetinaDataset, get_transforms
 
-def prepare_dataloaders(data_dir, batch_size):
-    transform = get_transforms()
-
+def prepare_dataloaders(data_dir, batch_size, train_transform, val_transform):
     train_dataset = RetinaDataset(
         image_dir=os.path.join(data_dir, "train/images"),
         mask_dir=os.path.join(data_dir, "train/masks"),
         fov_dir=os.path.join(data_dir, "train/fov"),
-        transform=transform
+        transform=train_transform
     )
 
-    test_dataset = RetinaDataset(
+    val_dataset = RetinaDataset(
         image_dir=os.path.join(data_dir, "test/images"),
         mask_dir=os.path.join(data_dir, "test/masks"),
         fov_dir=os.path.join(data_dir, "test/fov"),
-        transform=transform
+        transform=val_transform
     )
 
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
 
-    return train_loader, test_loader
+    return train_loader, val_loader
+
+def prepare_test_dataloader(data_dir, batch_size, transform):
+    dataset = RetinaDataset(
+        image_dir=os.path.join(data_dir, "images"),
+        mask_dir=os.path.join(data_dir, "masks"),
+        fov_dir=os.path.join(data_dir, "fov"),
+        transform=transform
+    )
+    loader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
+    return loader
