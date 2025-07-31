b0dd9224543e294bed1921b03771f373aba0477b
mitrovicjj <mitrovicj1012@gmail.com>
2025-07-31 17:20:46 +0200

    New utils for SAM

---
+++ data/samutils.py
@@ -0,0 +1,33 @@
+import os
+from torch.utils.data import DataLoader
+from data.samdataset2 import RetinaDatasetSAM2, get_sam_transforms
+
+def prepare_dataloaders(data_dir, batch_size, train_transform, val_transform):
+    train_dataset = RetinaDatasetSAM2(
+        image_dir=os.path.join(data_dir, "train/images"),
+        mask_dir=os.path.join(data_dir, "train/masks"),
+        fov_dir=os.path.join(data_dir, "train/fov"),
+        transform=train_transform
+    )
+
+    val_dataset = RetinaDatasetSAM2(
+        image_dir=os.path.join(data_dir, "test/images"),
+        mask_dir=os.path.join(data_dir, "test/masks"),
+        fov_dir=os.path.join(data_dir, "test/fov"),
+        transform=val_transform
+    )
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+
+    return train_loader, val_loader
+
+def prepare_test_dataloader(data_dir, batch_size, transform):
+    dataset = RetinaDatasetSAM2(
+        image_dir=os.path.join(data_dir, "images"),
+        mask_dir=os.path.join(data_dir, "masks"),
+        fov_dir=os.path.join(data_dir, "fov"),
+        transform=transform
+    )
+    loader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
+    return loader
